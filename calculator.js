66d7cab601408bd562cf06279f2b043449f84506
Greyhertz <greyhert120@gmail.com>
2024-06-23 03:19:43 +0100

    hello world

---
+++ calculator.js
@@ -0,0 +1,19 @@
+
+
+function appendToDisplay(input) {
+  const display = document.getElementById("display");
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'error'
+  }
+}
+
---
+++ players.js
@@ -0,0 +1,5 @@
+const inputText = document.getElementById('input-text');
+const textDescription = document.getElementById('text-description');
+const taskDate = document.getElementById('task-date');
+
+const submitTask = document.getElementById('submit-task');
